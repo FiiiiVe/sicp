5ceb657a3eb8ac805cb899cfb1a1aa97dd70e2e3
Chan <czx_4@163.com>
2023-06-02 23:40:43 +0800

    day?+1

--- sicp.scm
+++ sicp.scm
@@ -145,3 +145,48 @@
               ((not (pair? x)) 1)
               (else (+ (count-leaves (car x))
                        (count-leaves (cdr x))))))
+;;p78 序列操作
+(define (filter predicate sequence)
+  (cond ((null? sequence) nil)
+        ((predicate (car sequence))
+         (cons (car sequence)
+               (filter predicate (cdr sequence))))
+        (else (filter predicate (cdr sequence)))))
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+(define (enumerate-interval low high)
+  (if (> low high)
+      nil
+      (cons low (enumerate-interval (+ low 1) high))))
+(define (enumerate-tree tree)
+  (cond ((null? tree) nil)
+        ((not (pair? tree)) (list tree))
+        (else (append (enumerate-tree (car tree))
+                      (enumerate-tree (cdr tree))))))
+
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+      nil
+      (cons (accumulate op init (map (lambda (x) (car x)) seqs))
+            (accumulate-n op init (map (lambda (x) (cdr x)) seqs)))))
+
+(define (display-matrix m)
+  (display "(")
+  (define (iter m)
+    (if (null? m)
+        nil
+        (begin
+          (newline)
+          (display "  ")
+          (display (car m))
+          (iter (cdr m)))))
+  (iter m)
+  (newline)
+  (display ")")
+  (newline))
+(define (display-n x)
+  (display x)
+  (newline))
