7bf9340a679e20e3f76b66f1ff3fa59d451b3fe0
chan <czx_4@163.com>
2023-08-11 13:14:17 +0800

    day??+11

--- 3/2.scm
+++ 3/2.scm
@@ -3,24 +3,48 @@
 
 (define (make-monitored f)
   (define (inner c)
-    (define how-many-calls? c)
-    (define reset-count
-      (begin
-        (set! c 0)))
+    ;; wrong
+    ;; (define reset-count
+    (define (reset-count)
+        (set! c 0)) 
     (define (call m)
       (begin
         (set! c (+ c 1))
-        (display c)
         (f m)))
     (define (mf command)
-      (cond ((eq? command 'how-many-calls?) how-many-calls?)
-            ((eq? command 'reset-count) reset-count)
+      (cond ((eq? command 'how-many-calls?) c)
+            ;; wrong and i dont know why??
+            ;; ((eq? command 'reset-count) reset-count)
+            ;; !!!maybe reset-count should be a procedure instead of a vari
+
+            ((eq? command 'reset-count) (reset-count))
+            ;; correct and i dont know why
+            ;; ((eq? command 'reset-count) (set! c 0))
             (else (call command))))
     mf)
   (inner 0))
 
+#|
+(define (make-monitored function)
+  (define (times-called c)
+    (define (mf message)
+      (cond ((eq? message 'how-many-calls?) c)
+            ((eq? message 'reset-count) (set! c 0))
+            (else (set! c (+ c 1))
+                  (function message))))
+    mf)
+  (times-called 0)) 
+
+|#
 (define s (make-monitored sqrt))
 
-(s 100)
+;; (display (s 'how-many-calls?))
 
-(s 'how-many-calls?)
+(display (s 100))(newline)
+
+(display (s 'how-many-calls?))(newline)
+
+(display (s 'reset-count))(newline)
+(s 100)
+(s 144)
+(display (s 'how-many-calls?))(newline)
