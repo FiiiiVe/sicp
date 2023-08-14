bc4d6f1406cacb1347b761e2dac1c8eeafb7891e
chan <czx_4@163.com>
2023-08-14 15:49:43 +0800

    day??+12

--- sicp.scm
+++ sicp.scm
@@ -339,3 +339,18 @@
         (adjoin-set-tree (make-leaf (car pair)
                                     (cadr pair))
                          (make-leaf-set (cdr pairs))))))
+
+;; p157 随机数
+(define (random-in-range low high)
+  (let ((range (- high low)))
+    (+ low (random range))))
+
+(define (monte-carlo trials experiment)
+  (define (iter trials-remaining trials-passed)
+    (cond ((= trials-remaining trials-passed)
+           (/ trials-passed trials))
+          ((experiment)
+           (iter (- trials-remaining 1) (+ trials-passed 1)))
+          (else
+           (iter (- trials-remaining 1) trials-passed))))
+  (iter trials 0))
