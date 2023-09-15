baa283ad73a30016d9918bfc092d955cbf8404e9
chan <czx_4@163.com>
2023-09-15 13:13:31 +0800

    day??+26

---
+++ 3/26.scm
@@ -0,0 +1,8 @@
+#|
+修改assoc、insert、look-up
+将原本的record结构(table (a 1) (b 2) (c 3))
+换为(table (a 1) left right)
+
+将原本的table结构(table (tableA (a 1)) (tableB (b 1)))
+换为(table (tableA left right))
+|#
---
+++ 3/27.scm
@@ -0,0 +1,5 @@
+#|
+图就不画了
+为什么是正比于n，前提是memoize是常数级别，用树结构就不行，还是需要散列表，经典的空间换时间
+memo-fib定义成(memoize fib)不可以，是因为fib计算没有用memo，memoize fib等于没有memo。。。
+|#
