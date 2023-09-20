81732599fed171b8bd5afee1389cffc5fe1f2940
Chan <czx_4@163.com>
2023-09-21 00:02:33 +0800

    repair days+25 finally we make it

--- 3/25.scm
+++ 3/25.scm
@@ -10,6 +10,10 @@
       (cond ((null? records) #f)
             ((equal? key (caar records)) (car records))
             (else (assoc key (cdr records)))))
+    (define (read-value record)
+      (if (null? record)
+          (error "read null value")
+          (cdr record)))
     (define (is-table? l)
       (or (null? (cdr l)) (pair? (cdr l))))
     (define (lookup key-list)
@@ -21,49 +25,57 @@
             (let ((key (car keys))
                   (record-or-next-subtable
                    (assoc (car keys) (cdr current-table))))
-              (if record-or-next-subtable
-                  (if (is-table? record-or-next-subtable)
-                      ;; lookup table--check
-                      (iter (cdr keys) record-or-next-subtable)
-                      ;; lookup record---check
-                      (cdr record-or-next-subtable)
-                      )
-                  (if (null? (cdr keys))
-                      (error "no value exist")
-                      (error "remain keys not found"))))))
+              (cond ((not record-or-next-subtable)
+                     ;; -- check
+                     (error "read null record or table: ~a" key))
+                    ((is-table? record-or-next-subtable)
+                     ;; lookup table --check
+                     (iter (cdr keys) record-or-next-subtable))
+                    ((not (null? (cdr keys)))
+                     ;; -- check
+                     (error "should read table, but now find record, at key: ~a" key))
+                    ;; lookup record -- check
+                    (else (cdr record-or-next-subtable)))
+              )))
       (iter key-list local-table))
     (define (insert! key-list value)
       (define (iter keys current-subtable)
-        (cond ((null? keys)
-               ;; only first call happen
-               ;; or wrong call which want to
-               ;; insert value to a table instead of record
-               (error "insert null keys error"))
-              (else
-               (let ((key (car keys))
-                     (record-or-next-subtable
-                      (assoc (car keys) (cdr current-subtable))))
-                 ;; next layer not exist, insert record or table
-                 (cond ((not record-or-next-subtable)
-                        (if (null? (cdr keys))
-                            ;; insert record--check
-                            (set-cdr! current-subtable (cons (cons key value)
-                                                             (cdr current-subtable)))
-                            ;; insert table--check
-                            (begin 
-                              (set-cdr! current-subtable
-                                        (cons (list key) (cdr current-subtable)))
-                              (iter (cdr keys) (cadr current-subtable)))
-                            ))
-                       ;; next layer exist, update record or iter next table
-                       (else
-                        (if (is-table? record-or-next-subtable)
-                            ;; iter next table--check
-                            (iter (cdr keys) record-or-next-subtable)
-                            ;; update record--check
-                            ;; todo check is record or table
-                            (set-cdr! record-or-next-subtable value)
-                            )))))))
+        (if (null? keys)
+            ;; only first call happen
+            ;; or wrong call which want to
+            ;; insert value to a table instead of record
+            (error "insert null keys error")
+            (let ((key (car keys))
+                  (record-or-next-subtable
+                   (assoc (car keys) (cdr current-subtable))))
+              ;; next layer not exist, insert record or table
+              (if (null? (cdr keys))
+                  ;; shoud insert/update record
+                  (cond ((not record-or-next-subtable)
+                         ;; insert -- check
+                         (set-cdr! current-subtable (cons (cons key value)
+                                                          (cdr current-subtable))))
+                        ((is-table? record-or-next-subtable)
+                         ;; -- check
+                         (error "should find record, but find table, key:~a" key))
+                        (else
+                         ;; update -- check
+                         (set-cdr! record-or-next-subtable value)
+                         ))
+                  ;; should iter next layer
+                  (cond ((not record-or-next-subtable)
+                         ;; insert&iter next layer -- check
+                         (begin
+                           (set-cdr! current-subtable
+                                     (cons (list key) (cdr current-subtable)))
+                           (iter (cdr keys) (cadr current-subtable)))
+                         )
+                        ((is-table? record-or-next-subtable)
+                         ;; iter next layer -- check
+                         (iter (cdr keys) record-or-next-subtable))
+                        (else
+                         ;; -- check
+                         (error "should find table, but find record, key:~a" key)))))))
       (iter key-list local-table))
     (define (dispatch m)
       (cond ((eq? m 'lookup-proc) lookup)
@@ -81,7 +93,15 @@
 (display ((t 'lookup-proc) (list 'a 'b 'd)))(newline)
 ((t 'insert-proc) (list 'a 'b 'd) 'd2-value)
 (display ((t 'lookup-proc) (list 'a 'b 'd)))(newline)
+;; test insert table to record
+;; ((t 'insert-proc) (list 'a 'b 'd 'e) 'e-value)
 
-;; remain problem
-((t 'insert-proc) (list 'a 'b 'd 'e) 'e-value)
-(display ((t 'lookup-proc) (list 'a 'b 'd 'e)))(newline)
+;; test find record at table
+;; (display ((t 'lookup-proc) (list 'a 'b 'd 'e)))(newline)
+
+;; test read non-exist key
+;; (display ((t 'lookup-proc) (list 'a 'b 'cccc)))(newline)
+
+;; insert record to table
+;; ((t 'insert-proc) (list 'a 'b) 'b-value)
+
